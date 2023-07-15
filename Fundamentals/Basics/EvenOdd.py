0056411b453e1b89173267f44a86d8bdfd3eb81e
G17MSD <DMA001035@dixonsma.com>
2023-07-15 11:04:25 +0100

    Adding First file to github

---
+++ Fundamentals/Basics/EvenOdd.py
@@ -0,0 +1,13 @@
+'''a=int(input('Enter a number here: '))
+if (a%2==0):
+    print(a,'is an even number')
+elif (a%2!=0):
+    print(a,'is an odd number')'''
+
+'''for i in range(1,6,1):
+    print('Gurman')'''
+Sum=0
+for i in range(0,10,2):
+    Sum=Sum+i
+print(Sum)
+    
